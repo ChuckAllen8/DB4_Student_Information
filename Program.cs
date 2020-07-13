5ee077c645de3ca3a599e5f4d2b7eb44f435ad7d
Chuck Allen <ChuckAllen@QuickenLoans.com>
2020-07-13 16:47:16 -0400

    The initial commit of the project

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DB4_Student_Information
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StudentInformation app = new StudentInformation();
+            app.Start();
+        }
+    }
+}
---
+++ StudentInformation.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DB4_Student_Information
+{
+    class StudentInformation
+    {
+
+        private string[] names;
+        private string[] foods;
+        private string[] titles;
+
+        private string[] foodInputs;
+        private string[] titleInputs;
+        private string[] bothInputs;
+
+        private int selectedStudent;
+        private const int CLASS_SIZE = 14;
+
+        public StudentInformation()
+        {
+            names = new string[CLASS_SIZE] { "Chuck Allen", "Adam Gerecke", "Tyler Satkowiak", "Zach Scholts", "Kathryn Pidek", 
+                "Dave Gedert", "Eugene Hill", "David Heizer", "James Nofs", "Aaron Rudzki", "Adam Gerecke", "Brendan Burch", "Manny Salazar", "Eric Slutz" };
+
+            foods = new string[CLASS_SIZE] { "Reese's", "Milky Way Simply Caramel", "Reese's Fast Break", "All Reese's", "M&M's", 
+                "Snickers", "Kit Kat", "Combos", "Hershey's", "Candy Sticks", "Laffy Taffy", "Fudge", "Kit Kat", "Snickers" };
+
+            titles = new string[CLASS_SIZE] { "Resolution Analyst", "Desktop Support Technician", "Government Insuring Specialist", "Desktop Support Technician", 
+                "Desktop Support Technician", "Desktop Support Technician", "Desktop Support Technician", "Desktop Support Technician", "Desktop Support Technician", 
+                "Desktop Support Technician", "Desktop Support Technician", "Desktop Support Technician", "Desktop Support Technician", "Desktop Support Technician" };
+
+            foodInputs = new string[] { "FAVORITE CANDY", "CANDY", "FAV", "FAVORITE", "FOOD", "F", "C" };
+            titleInputs = new string[] { "PREVIOUS TITLE", "TITLE", "PREVIOUS", "PREV", "JOB", "P", "T" };
+            bothInputs = new string[] { "BOTH", "B", "ALL", "A" };
+        }
+
+        public void Start()
+        {
+            bool showAgain;
+
+            Console.Write("Welcome to our class! ");
+            do
+            {
+                Console.WriteLine("Which student would you like to learn more about?");
+                GetStudentChoice();
+
+                Console.WriteLine($"\nStudent {selectedStudent+1} is {names[selectedStudent]}. What would you like to know about {names[selectedStudent].Split(" ")[0]}?");
+                
+                switch (GetDataChoice())
+                {
+                    case 1:
+                        Console.WriteLine($"{names[selectedStudent].Split(" ")[0]}'s favorite candy is {foods[selectedStudent]}.");
+                        break;
+                    case 2:
+                        Console.WriteLine($"{names[selectedStudent].Split(" ")[0]}'s previous title was {titles[selectedStudent]}.");
+                        break;
+                    case 3:
+                        Console.WriteLine($"{names[selectedStudent].Split(" ")[0]}'s favorite candy is {foods[selectedStudent]} and previous title was {titles[selectedStudent]}.");
+                        break;
+                    default:
+                        break;
+                }
+
+                Console.WriteLine();
+                showAgain = RunAgain();
+            } while (showAgain);
+        }
+
+        private void GetStudentChoice()
+        {
+            Console.Write($"Enter \"(S)how All\" or a number 1-{names.Length}: ");
+            string input = Console.ReadLine();
+            while (!int.TryParse(input, out selectedStudent) || (selectedStudent < 1 || selectedStudent > names.Length))
+            {
+                if (input.ToUpper() == "SHOW ALL" || input.ToUpper() == "S")
+                {
+                    Console.WriteLine("The students are:");
+                    for(int index = 0; index < names.Length; index++)
+                    {
+                        Console.WriteLine($"{index+1}. {names[index]}");
+                    }
+                    Console.Write($"Enter \"(S)how All\" or a number 1-{names.Length}: ");
+                }
+                else
+                {
+                    Console.Write($"\nThat student does not exist. Please try again. \nEnter a number, 1-{names.Length}: ");
+                }
+                input = Console.ReadLine();
+            }
+            selectedStudent--; //sets it to the index
+        }
+
+        private int GetDataChoice()
+        {
+            Console.Write("Enter \"Favorite Candy\" or \"Previous Title\" or \"Both\": ");
+            string userChoice = Console.ReadLine().ToUpper();
+            while (!foodInputs.Contains(userChoice) && !titleInputs.Contains(userChoice) && !bothInputs.Contains(userChoice))
+            {
+                Console.Write("\n\nThat data does not exist. Please try again. \nEnter \"Favorite Candy\" or \"Previous Title\" or \"Both\": ");
+                userChoice = Console.ReadLine().ToUpper();
+            }
+
+            Console.WriteLine();
+
+            if (foodInputs.Contains(userChoice))
+            {
+                return 1;
+            }
+            else if (titleInputs.Contains(userChoice))
+            {
+                return 2;
+            }
+            else
+            {
+                return 3;
+            }
+        }
+
+        private bool RunAgain()
+        {
+            Console.Write("Would you like to know about another student? (enter \"yes\" or \"no\"): ");
+            string choice = Console.ReadLine().ToUpper();
+
+            while (!((choice == "YES") || (choice =="NO") || (choice == "Y") || (choice == "N")))
+            {
+                Console.Write("That was not a valid option. Enter \"yes\" or \"no\"): ");
+                choice = Console.ReadLine().ToUpper();
+            }
+
+            Console.WriteLine();
+
+            if(choice == "YES" || choice == "Y")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
